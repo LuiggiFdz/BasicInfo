8c739d46d1b4fa6f668f91d5ef850e2bc9015588
Luiggi Fernández <luiggimoney@gmail.com>
2018-05-07 20:34:29 -0500

    Create Hourglass.java
    The input is a 2D array with ints, the max sum of an hourglass in the array is the output.

---
+++ Hourglass.java
@@ -0,0 +1,41 @@
+import java.util.*;
+/**
+ * @author LuiggiFernandez
+ */
+public class Hourglass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int max = 0;
+        for (int i=0; i < 4; i++) {
+            for (int j=0; j < 4; j++) {
+                if ((arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]) > max) {
+                    max = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
+/*  Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+
+*/
+
+/* Sample Output
+
+19
+
+*/
